f37982b8c4d56c0f680c228f22a9bf8d732c2f8a
D-Sushma <99126858+D-Sushma@users.noreply.github.com>
2022-07-26 15:03:32 +0530

    react router dom....

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 //. task--> amazon img & all ko click krne pr 1st page open hona (includee header.js, subheader.js, card.js)
 //1.create Home.js compo than import in app.js than inside home.js import(all header, subheader, card.js compo)
 // -->>> qki jb amazon img & all ko  click krne pr hamara home.js open hoga & isme sare compo show krenge---------
-//2. ctrl+c to stop server & install nmp i react-router-dom than check,it show on package.json than open server with npm start
+//2. ctrl+c to stop server & install npm i react-router-dom than check,it show on package.json than open server with npm start
 // --->>>>> import router with new version 
 // 3. make router ------>>>>>
 // "/"-->1st page--> ye hame direct show krega brouser me
